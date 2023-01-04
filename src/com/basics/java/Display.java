df58b775a6ba15cd44f7c83639f75bb8b9a88714
VigneshNallasivam <vickyshanestark@gmail.com>
2023-01-04 15:51:59 +0530

    Added : MASTER - Displaying the Welcome Message For the Program

---
+++ src/com/basics/java/Display.java
@@ -0,0 +1,10 @@
+package com.basics.java;
+
+public class Display {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome  To Employee Payroll Service Program Simulation");
+	}
+
+}
